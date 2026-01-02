db45835896b110132a7e9cc9268fc0b2a5f42693
krishna singh <0.krishna1120@gmail.com>
2026-01-02 22:48:56 +0530

    feat: implement routing and layout structure in App component

--- movie-booking-app-client/src/App.jsx
+++ movie-booking-app-client/src/App.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import { Route, Routes } from 'react-router-dom'
+import Movies from './pages/Movies'
+import MovieDetails from './pages/MovieDetails'
+import SeatLayout from './pages/SeatLayout'
 
 const App = () => {
   return (
     <div>
-      
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/movies' element={<Movies />} />
+        <Route path='/movies/:id' element={<MovieDetails />} />
+        <Route path='/movies/:id/:date' element={<SeatLayout />} />
+      </Routes>
     </div>
   )
 }
