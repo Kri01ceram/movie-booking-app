63c2d9d18017debe77b3d7fdc1c16f3edfcc8425
krishna singh <0.krishna1120@gmail.com>
2026-01-03 13:22:17 +0530

    feat: enhance Navbar component with fixed positioning and responsive styling

--- movie-booking-app-client/src/components/Navbar.jsx
+++ movie-booking-app-client/src/components/Navbar.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div>
+    <div className='fixed top-0 left-0 z-50 w-full flex items-center justify-between px-6 md:px-16 lg:px-26 py-5'>
+      <Link className='max-md:flex-1'>
+      <img src="{assets.logo}" alt="" className='w-36 h-auto'/>
+      </Link>
       
     </div>
   )
