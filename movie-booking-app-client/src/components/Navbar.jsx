78f84bd745abe638c74c97ae7e3caa58358f7e87
krishna singh <0.krishna1120@gmail.com>
2026-01-03 13:26:53 +0530

    fix: add missing 'to' prop in Link component in Navbar

--- movie-booking-app-client/src/components/Navbar.jsx
+++ movie-booking-app-client/src/components/Navbar.jsx
@@ -5,9 +5,15 @@ import assets from '../assets/assets'
 const Navbar = () => {
   return (
     <div className='fixed top-0 left-0 z-50 w-full flex items-center justify-between px-6 md:px-16 lg:px-26 py-5'>
-      <Link className='max-md:flex-1'>
+      <Link to='/' className='max-md:flex-1'>
       <img src={assets.logo} alt="" className='w-36 h-auto'/>
       </Link>
+      <div>
+
+      </div>
+      <div>
+        
+      </div>
       
     </div>
   )
