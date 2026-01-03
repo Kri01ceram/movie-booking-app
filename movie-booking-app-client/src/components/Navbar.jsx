26e9ef429c1e3e53c06b5f84fe7514501a3b3d17
krishna singh <0.krishna1120@gmail.com>
2026-01-03 13:39:39 +0530

    fix: add missing XIcon import and correct Navbar structure

--- movie-booking-app-client/src/components/Navbar.jsx
+++ movie-booking-app-client/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import assets from '../assets/assets'
-import { MenuIcon, SearchIcon } from 'lucide-react'
+import { MenuIcon, SearchIcon, XIcon } from 'lucide-react'
 
 const Navbar = () => {
   return (
@@ -9,8 +9,14 @@ const Navbar = () => {
       <Link to='/' className='max-md:flex-1'>
       <img src={assets.logo} alt="" className='w-36 h-auto'/>
       </Link>
-      <div>
-          <Link ></Link>
+      <div className='max-md:absolute max-md:top-0 max-md: left-0 max-md:font-medium max-md:text-lg z-50 flex flex-col md: flex-row items-centermax-md:justify-center gap-8 min-md:px-8 py-3 max-md:h-screen min-md: rounded-full backdrop-blur bg-black/70 md:bg-white/10 md:border border-gray-300/20 overflow-hidden transition-[width] duration-300'>
+I
+        <XIcon className='md:hidden absolute top-6 right-6 w-6 h-6 cursor-pointer'/>
+          <Link to='/'>Home</Link>
+          <Link to='/movies'>Movies</Link>
+          <Link to='/theatres'>Theatres</Link>
+          <Link to='/releases'>Releases</Link>
+          <Link to='/favorites'>Favorites</Link>
       </div>
       <div className='flex items-center gap-8'>
         <SearchIcon className='max-md:hidden w-6 h-6 cursor-pointer'/>
