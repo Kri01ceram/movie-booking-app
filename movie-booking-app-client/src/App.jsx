f0f18194e52cdaa93fff8a50e56a6f18e4c487e0
krishna singh <0.krishna1120@gmail.com>
2026-01-03 13:03:51 +0530

    feat: enhance App component with admin route handling and Toaster integration

--- movie-booking-app-client/src/App.jsx
+++ movie-booking-app-client/src/App.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, useLocation } from 'react-router-dom'
 import Movies from './pages/Movies'
 import MovieDetails from './pages/MovieDetails'
 import SeatLayout from './pages/SeatLayout'
 import MyBookings from './pages/MyBookings' 
 import Favorite from './pages/Favorite'
+import {Toaster} from 'react-hot-toast'
 
 const App = () => {
+  const isAdminRoute = useLocation().pathname.startsWith('/admin')
   return (
-    <div>
-      <Navbar />
+    <>
+    <Toaster />
+      {!isAdminRoute && <Navbar />}
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/movies' element={<Movies />} />
@@ -20,7 +23,7 @@ const App = () => {
         <Route path='/my-bookings' element={<MyBookings />} />
         <Route path='/favorite' element={<Favorite />} />
       </Routes>
-    </div>
+    </>
   )
 }
 
