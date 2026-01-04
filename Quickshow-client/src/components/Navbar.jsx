7bffe0e9959795c56369b5a4c499d2dc871ffa8e
krishna singh <0.krishna1120@gmail.com>
2026-01-04 08:43:36 +0530

    feat: enhance Navbar with user authentication and sign-in functionality

--- Quickshow-client/src/components/Navbar.jsx
+++ Quickshow-client/src/components/Navbar.jsx
@@ -3,10 +3,13 @@ import { Link } from 'react-router-dom'
 import { assets } from '../assets/assets'
 import { useState } from 'react'
 import { MenuIcon, SearchIcon, XIcon } from 'lucide-react'
+import { useClerk, UserButton, useUser } from '@clerk/clerk-react'
 
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const {user} = useUser();
+  const {openSignIn} = useClerk();
   return (
     <div className='fixed top-0 left-0 z-50 w-full flex items-center justify-between px-6 md:px-16 lg:px-26 py-5'>
       <Link to='/' className='max-md:flex-1'>
@@ -29,9 +32,14 @@ const Navbar = () => {
       </div>
       <div className='flex items-center gap-8'>
         <SearchIcon className='max-md:hidden w-6 h-6 cursor-pointer'/>
-        <button className='px-4 py-1 sm:px-7 sm:py-2 bg-primary
+        {
+          !user ? (<button onClick={openSignIn} className='px-4 py-1 sm:px-7 sm:py-2 bg-primary
         hover:bg-primary-dull transition rounded-full font-medium
-        cursor-pointer'>Login</button>
+        cursor-pointer'>Login</button>) : (
+          <UserButton />
+        )
+        }
+        
      </div>
       <MenuIcon className='max-md:ml-4 md:hidden w-8 h-8 cursor-pointer' onClick={() => setIsOpen(true)} />
       
