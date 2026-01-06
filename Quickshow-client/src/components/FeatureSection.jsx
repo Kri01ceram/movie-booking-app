32e395ba91b842229074fd9883a36dbdf1aad262
krishna singh <0.krishna1120@gmail.com>
2026-01-06 18:01:53 +0530

    feat: implement FeatureSection component to display current movies

--- Quickshow-client/src/components/FeatureSection.jsx
+++ Quickshow-client/src/components/FeatureSection.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
+import { ArrowRight } from 'lucide-react'
+import { useNavigate } from 'react-router-dom'
 
 const FeatureSection = () => {
+    const navigate = useNavigate();
   return (
-    <div>
-      
+    <div className="px-6 md:px-16 1g:px-24 x1:px-44 overflow-hidden">
+      <div className="relative flex items-center justify-between pt-20 pb-10">
+        <p className="text-gray-300 font-medium text-lg">Now Showing</p>
+        <button
+          onClick={() => navigate("/movies")}
+          className="group flex items-center gap-2 text-sm text-gray-300"
+        >
+          View All
+          <ArrowRight className="group-hover: translate-x-0.5 transition w-4.5 h-4.5" />
+        </button>
+      </div>
+      <div></div>
+      <div></div>
     </div>
-  )
+  );
 }
 
 export default FeatureSection
--- Quickshow-client/src/pages/Home.jsx
+++ Quickshow-client/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import HeroSectio from '../components/HeroSectio'
+import FeatureSection from '../components/FeatureSection'
 
 const Home = () => {
   return (
     <>
       <HeroSectio />
+      <FeatureSection/>
     </>
   )
 }
