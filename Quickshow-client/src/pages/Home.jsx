3077d91cf4766cf6e65c996e723aa0b8c6970350
krishna singh <0.krishna1120@gmail.com>
2026-01-12 19:27:18 +0530

    feat: add TrailerSectio component to display movie trailers

--- Quickshow-client/src/pages/Home.jsx
+++ Quickshow-client/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import HeroSectio from '../components/HeroSectio'
 import FeatureSection from '../components/FeatureSection'
+import TrailerSectio from '../components/TrailerSectio'
 
 const Home = () => {
   return (
     <>
       <HeroSectio />
       <FeatureSection/>
+      <TrailerSectio/>
     </>
   )
 }
