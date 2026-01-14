d1bd265b99923d071df1d28bc58dd97201a0ddb4
krishna singh <0.krishna1120@gmail.com>
2026-01-14 19:10:03 +0530

    feat: implement MovieDetails component to fetch and display movie details

--- Quickshow-client/src/pages/MovieDetails.jsx
+++ Quickshow-client/src/pages/MovieDetails.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { dummyDateTimeData, dummyShowsData } from '../assets/assets';
 
 const MovieDetails = () => {
-  return (
+  const {id} = useParams();
+  const [show, setShow] = React.useState(null);
+  const getShow = async()=>{
+    const show = dummyShowsData.find((show)=> show._id === id);
+    setShow({
+      movie: show,
+      dateTime : dummyDateTimeData
+    });
+  }
+  useEffect(()=>{
+    getShow()
+  }, [id])
+  return show ? (
     <div>
       
     </div>
-  )
+  ) : <div>Loading...</div>
 }
 
 export default MovieDetails
